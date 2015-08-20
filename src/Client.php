4875d4d4d9cd8118ca0830f7b33e80837ad9cd2b
Jared King <j@jaredtking.com>
2015-08-20 14:27:26 -0500

    added tests for Client class

--- src/Client.php
+++ src/Client.php
@@ -2,137 +2,148 @@
 
 namespace Invoiced;
 
-use Exception;
 use InvalidArgumentException;
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\HandlerStack;
 
 class Client
 {
-	const API_BASE = 'https://api.invoiced.com';
+    const API_BASE = 'https://api.invoiced.com';
 
-	const VERSION = '0.0.1';
+    const VERSION = '0.0.1';
 
-	var $Customer;
-	var $Invoice;
-	var $Transaction;
+    public $Customer;
+    public $Invoice;
+    public $Transaction;
+    public $Plan;
+    public $Subscription;
 
-	/**
-	 * @var string
-	 */
-	private $apiKey;
+    /**
+     * @var string
+     */
+    private $apiKey;
 
-	/**
-	 * @var GuzzleHttp\Client
-	 */
-	private $client;
+    /**
+     * @var GuzzleHttp\Client
+     */
+    private $client;
 
-	/**
-	 * Instantiates a new client
-	 *
-	 * @param string $apiKey
-	 */
-	function __construct($apiKey)
-	{
-		if (empty($apiKey)) {
-			throw new InvalidArgumentException('You must provide an API Key');
-		}
+    /**
+     * Instantiates a new client.
+     *
+     * @param string             $apiKey
+     * @param GuzzleHttp\Handler $handler
+     */
+    public function __construct($apiKey, $handler = null)
+    {
+        if (empty($apiKey)) {
+            throw new InvalidArgumentException('You must provide an API Key');
+        }
 
-		$this->apiKey = $apiKey;
-		$this->client = new GuzzleClient(['base_uri' => self::API_BASE]);
+        // add any custom Guzzle HTTP handlers (useful for mocking)
+        $handlerStack = ($handler) ? HandlerStack::create($handler) : HandlerStack::create();
 
-		$this->Customer = new Customer($this);
-		$this->Invoice = new Invoice($this);
-		$this->Transaction = new Transaction($this);
-	}
+        $this->apiKey = $apiKey;
 
-	function apiKey()
-	{
-		return $this->apiKey;
-	}
+        $this->client = new GuzzleClient([
+            'base_uri' => self::API_BASE,
+            'handler' => $handlerStack, ]);
 
-	function request($method, $endpoint, $params = [])
-	{
-		$method = strtolower($method);
+        $this->Customer = new Customer($this);
+        $this->Invoice = new Invoice($this);
+        $this->Transaction = new Transaction($this);
+        $this->Plan = new Plan($this);
+        $this->Subscription = new Subscription($this);
+    }
 
-		$headers = [
-			'Content-Type' => 'application/json',
-			'User-Agent' => 'Invoiced PHP/'.self::VERSION
-		];
+    public function apiKey()
+    {
+        return $this->apiKey;
+    }
 
-		$query = ['envelope' => false];
+    public function request($method, $endpoint, $params = [])
+    {
+        $method = strtolower($method);
 
-		$request = [
-			'auth' => [$this->apiKey, ''],
-			'headers' => $headers,
-			'http_errors' => false
-		];
+        $headers = [
+            'Content-Type' => 'application/json',
+            'User-Agent' => 'Invoiced PHP/'.self::VERSION,
+        ];
 
-		// these methods have no request body
-		if ($method === 'get' || $method === 'delete') {
-			$query = array_replace($query, $params);
-		} else {
-			$request['json'] = $params;
-		}
+        $query = ['envelope' => false];
 
-		$request['query'] = $query;
+        $request = [
+            'auth' => [$this->apiKey, ''],
+            'headers' => $headers,
+            'http_errors' => false,
+        ];
 
-		try {
-			// perform the request
-			$response = $this->client->$method($endpoint, $request);
-		} catch (RequestException $e) {
-			throw new Error\ApiError('There was an error connecting to the Invoiced API.');
-		}
+        // these methods have no request body
+        if ($method === 'get' || $method === 'delete') {
+            $query = array_replace($query, $params);
+        } else {
+            $request['json'] = $params;
+        }
 
-		// validate response
-		$code = $response->getStatusCode();
-		$body = $response->getBody();
+        $request['query'] = $query;
 
-		if ($code >= 200 && $code < 400) {
-			$parsed = null;
+        try {
+            // perform the request
+            $response = $this->client->$method($endpoint, $request);
+        } catch (RequestException $e) {
+            throw new Error\ApiError('There was an error connecting to the Invoiced API.');
+        }
 
-			// expect a JSON response unless we received 204 No Content
-			if ($code != 204) {
-				try {
-					$parsed = json_decode($body, true);
-				} catch (Exception $e) {
-					throw $this->generalApiError($code, $body);
-				}
-			}
+        // validate response
+        $code = $response->getStatusCode();
+        $body = $response->getBody();
 
-			$parsedHeaders = [];
-			foreach ($response->getHeaders() as $k => $v) {
-				$parsedHeaders[$k] = implode(', ', $v);
-			}
+        if ($code >= 200 && $code < 400) {
+            $parsed = null;
 
-			return [
-				'code' => $code,
-				'headers' => $parsedHeaders,
-				'body' => $parsed
-			];
-		} else {
-			try {
-				$error = json_decode($body, true);
-			} catch (Exception $e) {
-				throw $this->generalApiError($code, $body);
-			}
+            // expect a JSON response unless we received 204 No Content
+            if ($code != 204) {
+                $parsed = json_decode($body, true);
 
-			if ($code == 401) {
-				throw new Error\AuthenticationError($error['message'], $code, $error);
-			} else if (in_array($code, [400, 403, 404])) {
-				throw new Error\InvalidRequest($error['message'], $code, $error);
-			} else if ($code == 500) {
-				throw new Error\ApiError($error['message'], $code, $error);
-			} else {
-				throw $this->generalApiError($code, $body);
-			}
-		}
+                if ($parsed === null) {
+                    throw new Error\ApiError("Could not decode JSON of $code response: $body", $code);
+                }
+            }
 
-		return $response;
-	}
+            $parsedHeaders = [];
+            foreach ($response->getHeaders() as $k => $v) {
+                $parsedHeaders[$k] = implode(', ', $v);
+            }
 
-	private function generalApiError($code, $body)
-	{
-		return new Error\ApiError("API Error $code - $body", $code);
-	}
+            return [
+                'code' => $code,
+                'headers' => $parsedHeaders,
+                'body' => $parsed,
+            ];
+        } else {
+            $error = json_decode($body, true);
+
+            if ($error === null) {
+                throw $this->generalApiError($code, $body);
+            }
+
+            if ($code == 401) {
+                throw new Error\AuthenticationError($error['message'], $code, $error);
+            } elseif (in_array($code, [400, 403, 404])) {
+                throw new Error\InvalidRequest($error['message'], $code, $error);
+            } elseif ($code == 500) {
+                throw new Error\ApiError($error['message'], $code, $error);
+            } else {
+                throw $this->generalApiError($code, $body);
+            }
+        }
+
+        return $response;
+    }
+
+    private function generalApiError($code, $body)
+    {
+        return new Error\ApiError("API Error $code - $body", $code);
+    }
 }
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,28 +1,159 @@
 <?php
 
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Exception\RequestException;
 use Invoiced\Client;
 
 class ClientTest extends PHPUnit_Framework_TestCase
 {
-	function testNoApiKey()
-	{
-		$this->setExpectedException('Exception');
+    public function testNoApiKey()
+    {
+        $this->setExpectedException('InvalidArgumentException');
 
-		$client = new Client('');
-	}
+        $client = new Client('');
+    }
 
-	function testApiKey()
-	{
-		$client = new Client('test');
-		$this->assertEquals('test', $client->apiKey());
-	}
+    public function testApiKey()
+    {
+        $client = new Client('test');
+        $this->assertEquals('test', $client->apiKey());
+    }
 
-	function testRequest()
-	{
-		$client = new Client('API_KEY');
+    public function testRequest()
+    {
+        $mock = new MockHandler([
+            new Response(200, ['X-Foo' => 'Bar'], '{"test":true}'),
+        ]);
 
-		$response = $client->request('GET', '/invoices', ['per_page' => 3]);
-		
-		print_r($response);
-	}
+        $client = new Client('API_KEY', $mock);
+
+        $response = $client->request('GET', '/invoices', ['per_page' => 3]);
+
+        $expected = [
+            'code' => 200,
+            'headers' => [
+                'X-Foo' => 'Bar',
+            ],
+            'body' => [
+                'test' => 1,
+            ],
+        ];
+        $this->assertEquals($expected, $response);
+    }
+
+    public function testRequestPost()
+    {
+        $mock = new MockHandler([
+            new Response(201, ['X-Foo' => 'Bar'], '{"test":true}'),
+        ]);
+
+        $client = new Client('API_KEY', $mock);
+
+        $response = $client->request('POST', '/invoices', ['customer' => 123]);
+
+        $expected = [
+            'code' => 201,
+            'headers' => [
+                'X-Foo' => 'Bar',
+            ],
+            'body' => [
+                'test' => 1,
+            ],
+        ];
+        $this->assertEquals($expected, $response);
+    }
+
+    public function testRequestInvalidJson()
+    {
+        $this->setExpectedException('Invoiced\\Error\\ApiError');
+
+        $mock = new MockHandler([
+            new Response(200, [], 'not valid json'),
+        ]);
+
+        $client = new Client('API_KEY', $mock);
+
+        $client->request('GET', '/invoices');
+    }
+
+    public function testRequestAuthError()
+    {
+        $this->setExpectedException('Invoiced\\Error\\AuthenticationError');
+
+        $mock = new MockHandler([
+            new Response(401, [], '{"error":"invalid_request","message":"invalid api key"}'),
+        ]);
+
+        $client = new Client('API_KEY', $mock);
+
+        $client->request('GET', '/invoices');
+    }
+
+    public function testRequestInvalid()
+    {
+        $this->setExpectedException('Invoiced\\Error\\InvalidRequest');
+
+        $mock = new MockHandler([
+            new Response(400, [], '{"error":"invalid_request","message":"not found"}'),
+        ]);
+
+        $client = new Client('API_KEY', $mock);
+
+        $client->request('GET', '/invoices');
+    }
+
+    public function testRequestApiError()
+    {
+        $this->setExpectedException('Invoiced\\Error\\ApiError');
+
+        $mock = new MockHandler([
+            new Response(500, [], '{"error":"api","message":"idk"}'),
+        ]);
+
+        $client = new Client('API_KEY', $mock);
+
+        $client->request('GET', '/invoices');
+    }
+
+    public function testRequestGeneralApiError()
+    {
+        $this->setExpectedException('Invoiced\\Error\\ApiError');
+
+        $mock = new MockHandler([
+            new Response(502, [], '{"error":"api","message":"idk"}'),
+        ]);
+
+        $client = new Client('API_KEY', $mock);
+
+        $client->request('GET', '/invoices');
+    }
+
+    public function testRequestApiErrorInvalidJson()
+    {
+        $this->setExpectedException('Invoiced\\Error\\ApiError');
+
+        $mock = new MockHandler([
+            new Response(500, [], 'not valid json'),
+        ]);
+
+        $client = new Client('API_KEY', $mock);
+
+        $client->request('GET', '/invoices');
+    }
+
+    public function testRequestConnectionError()
+    {
+        $this->setExpectedException('Invoiced\\Error\\ApiError');
+
+        $request = new Request('GET', 'https://api.invoiced.com');
+        $mock = new MockHandler([
+            new RequestException('Could not connect', $request),
+        ]);
+
+        $client = new Client('API_KEY', $mock);
+
+        $client->request('GET', '/invoices');
+    }
 }
