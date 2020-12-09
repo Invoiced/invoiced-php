ec390c047cd476ca44b8086b7d030c635c565c54
Jared King <jaredtking@users.noreply.github.com>
2020-12-09 16:35:34 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Client.php
+++ src/Client.php
@@ -115,7 +115,7 @@ class Client
 
         $this->client = new GuzzleClient([
             'base_uri' => $this->endpoint(),
-            'handler' => $handlerStack,
+            'handler'  => $handlerStack,
         ]);
 
         // object endpoints
@@ -174,13 +174,13 @@ class Client
         $method = strtolower($method);
 
         $request = [
-            'auth' => [$this->apiKey, ''],
-            'headers' => $this->buildHeaders($opts),
-            'query' => [],
-            'http_errors' => false,
-            'verify' => $this->caBundleFile,
+            'auth'            => [$this->apiKey, ''],
+            'headers'         => $this->buildHeaders($opts),
+            'query'           => [],
+            'http_errors'     => false,
+            'verify'          => $this->caBundleFile,
             'connect_timeout' => self::CONNECT_TIMEOUT,
-            'read_timeout' => self::READ_TIMEOUT,
+            'read_timeout'    => self::READ_TIMEOUT,
         ];
 
         // these methods have no request body
@@ -219,9 +219,9 @@ class Client
             }
 
             return [
-                'code' => $code,
+                'code'    => $code,
                 'headers' => $parsedHeaders,
-                'body' => $parsed,
+                'body'    => $parsed,
             ];
         } else {
             $error = json_decode($body, true);
@@ -255,7 +255,7 @@ class Client
     {
         $headers = [
             'Content-Type' => 'application/json',
-            'User-Agent' => 'Invoiced PHP/'.self::VERSION,
+            'User-Agent'   => 'Invoiced PHP/'.self::VERSION,
         ];
 
         if (isset($opts['idempotency_key']) && $opts['idempotency_key']) {
