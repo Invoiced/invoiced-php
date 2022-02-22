0cc0fd217e971c746db42ce900aaac50a45b104c
Jared King <jared@invoiced.com>
2022-02-22 13:11:34 -0600

    bump version

--- src/Client.php
+++ src/Client.php
@@ -13,7 +13,7 @@ class Client
     const API_BASE = 'https://api.invoiced.com';
     const API_BASE_SANDBOX = 'https://api.sandbox.invoiced.com';
 
-    const VERSION = '2.2.0';
+    const VERSION = '2.3.0';
 
     const CONNECT_TIMEOUT = 30;
     const READ_TIMEOUT = 60;
