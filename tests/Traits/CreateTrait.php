af8fb977373de4b72edb9630c79e2cddde5e0486
Jared King <jared@invoiced.com>
2023-02-21 10:29:51 -0600

    Fix PHPStan errors

--- tests/Traits/CreateTrait.php
+++ tests/Traits/CreateTrait.php
@@ -3,6 +3,7 @@
 namespace Invoiced\Tests\Traits;
 
 use GuzzleHttp\Psr7\Response;
+use Invoiced\BaseObject;
 
 trait CreateTrait
 {
@@ -14,6 +15,7 @@ trait CreateTrait
         $client = $this->makeClient(new Response(201, [], '{"id":456}'));
 
         $class = static::OBJECT_CLASS;
+        /** @var BaseObject $newObj */
         $newObj = (new $class($client))->create(['name' => 'Nancy']); /* @phpstan-ignore-line */
 
         $this->assertInstanceOf(static::OBJECT_CLASS, $newObj);
